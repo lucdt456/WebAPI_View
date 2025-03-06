a1aa86451f31a74a76de9534d1af6f97220970d3
lucdt456 <lucdt456@gmail.com>
2025-03-06 16:10:57 +0700

    Create sản phẩm front-end

--- WebAPIView/wwwroot/js/apiProduct.js
+++ WebAPIView/wwwroot/js/apiProduct.js
@@ -1,10 +1,14 @@
-function loadProducts() {
+$(function () {
+    loadproducts();
+});
+function loadproducts() {
     $.ajax({
         url: 'https://localhost:7034/api/Product',
         type: 'GET',
         dataType: 'json',
         success: function (response) {
             console.log("Dữ liệu nhận từ API:", response);
+            $("#product-list").empty();
             $.each(response, function (index, product) {
                 $("#product-list").append(
                     `<tr>
@@ -35,6 +39,92 @@ function loadProducts() {
         }
     });
 }
-$(document).ready(function () {
-    loadProducts();
+
+$("#create").click(function () {
+    /*console.log('Tạo 1 sản phẩm')*/
+    let isValid = validateInput();
+    if (isValid == true) {
+        let product = {
+            name: $("#name").val(),
+            price: $("#price").val(),
+            image: $("#imageFile").val().split("\\").pop(),
+            quantity: $("#quantity").val(),
+            brandId: $("#brandid").val(),
+            categoryId: $("#categoryid").val(),
+            description: $("#description").val()
+        };
+        productJson = JSON.stringify(product);
+        $.ajax({
+            url: 'https://localhost:7034/api/Product',
+            type: "POST",
+            contentType: "application/json",
+            data: productJson,
+            success: function (response) {
+                alert("Tạo sản phẩm thành công!");
+                loadproducts();
+                console.log(response);
+            },
+            error: function (xhr, status, error) {
+                alert("Lỗi tạo sản phẩm: " + xhr.responseText);
+                console.error(error);
+            }
+        })
+    }
 });
+
+function validateInput() {
+    $(".error-message").text("");
+    let isValid = true;
+
+    $(".form .form-control").each(function () {
+        if ($(this).val().trim() === "") {
+            $(this).next(".error-message").text("Không được để trống")
+            isValid = false;
+        } 
+    })
+
+    let name = $("#name").val();
+    if (name.length > 20) {
+        $("#name").next(".error-message").text("Không được nhập quá 20 ký tự !!!");
+        isValid = false;
+    }
+
+    let price = parseFloat($("#price").val());
+    if (price < 0) {
+        $("#price").next(".error-message").text("Giá sản phẩm phải >= 0");
+        isValid = false;
+    }
+
+    let quantity = parseFloat($("#quantity").val());
+    if (quantity != parseInt($("#quantity").val())) {
+        $("#quantity").next(".error-message").text("Vui lòng nhập số nguyên");
+        isValid = false;
+    }
+    if (quantity < 0) {
+        $("#quantity").next(".error-message").text("Số lượng sản phẩm phải >= 0");
+        isValid = false;
+    }
+
+    let brandId = $("#brandid").val();
+    if (brandId === "") {
+        $("#brandid").next(".error-message").text("Vui lòng chọn thương hiệu");
+        isValid = false;
+    }
+
+    let categoryId = $("#categoryid").val();
+    if (categoryId === "") {
+        $("#categoryid").next(".error-message").text("Vui lòng chọn loại sản phẩm");
+        isValid = false;
+    }
+
+    let imageName = $("#imageFile").val().split("\\").pop();
+    if (imageName === "") {
+        $("#imageFile").next(".error-message").text("Vui lòng thêm file ảnh");
+        isValid = false;
+    }
+    return isValid;
+}
+
+$("#test").click(function () {
+    validateInput();
+})

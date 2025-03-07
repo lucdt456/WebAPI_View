903af842e81b99ce18b0c9ef1f48cfc8921e4846
lucdt456 <lucdt456@gmail.com>
2025-03-07 10:55:02 +0700

    Thêm phần update với delete

--- WebAPIView/wwwroot/js/apiProduct.js
+++ WebAPIView/wwwroot/js/apiProduct.js
@@ -1,6 +1,8 @@
 $(function () {
     loadproducts();
 });
+
+//Get
 function loadproducts() {
     $.ajax({
         url: 'https://localhost:7034/api/Product',
@@ -26,7 +28,7 @@ function loadproducts() {
                             <button class="btn btn-warning me-2" onclick="editProduct(${product.id})">
                                 <i class="bi bi-pencil-square"></i>
                             </button>
-                            <button class="btn btn-danger" onclick="editDelete(${product.id})">
+                            <button class="btn btn-danger" onclick="deleteProduct(${product.id})">
                                 <i class="bi bi-x-circle text-black"></i>
                             </button>
                         </td>
@@ -40,6 +42,8 @@ function loadproducts() {
     });
 }
 
+
+// Create
 $("#create").click(function () {
     /*console.log('Tạo 1 sản phẩm')*/
     let isValid = validateInput();
@@ -69,19 +73,162 @@ $("#create").click(function () {
                 console.error(error);
             }
         })
+    } else {
+        console.log("Validate false rồi")
+        console.log($("#imageTextNameFileHidden").val())
     }
 });
 
+
+//Edit
+function editProduct(id) {
+    document.getElementById("create").style.display = "none";
+    document.getElementById("update").style.display = "inline";
+    document.getElementById("backToCreate").style.display = "inline";
+
+    $.ajax({
+        url: `https://localhost:7034/api/Product/${id}`,
+        type: 'GET',
+        dataType: 'json',
+        success: function (product) {
+            $("#idProduct").val(product.id);
+            $("#name").val(product.name);
+            $("#price").val(product.price);
+            $("#quantity").val(product.quantity);
+            $("#description").val(product.description);
+            $("#brandid option").each(function () {
+                if ($(this).text() === product.brand) {
+                    $("#brandid").val($(this).val());
+                }
+            });
+            $("#categoryid option").each(function () {
+                if ($(this).text() === product.category) {
+                    $("#categoryid").val($(this).val());
+                }
+            });
+            $("#imageTextNameFileHidden").val(product.image)
+            $("#imageProduct").html(
+                `<img src="/Imgs/Products/${product.image}" width="100px" />`
+            )
+        },
+        error: function (xhr, status, error) {
+            alert("Lỗi khi lấy sản phẩm: " + xhr.responseText);
+            console.error(error);
+        }
+    })
+}
+
+// Button lưu update
+$("#update").click(function () {
+    let isValid = validateInput();
+    if (isValid == true) {
+        let id = $("#idProduct").val();
+        let imageName = $("#imageTextNameFileHidden").val();
+        if ($("#imageFile").val().split("\\").pop() != "") {
+            imageName = $("#imageFile").val().split("\\").pop();
+        }
+
+        let product = {
+            id: $("#idProduct").val(),
+            name: $("#name").val(),
+            price: $("#price").val(),
+            image: imageName,
+            quantity: $("#quantity").val(),
+            brandId: $("#brandid").val(),
+            categoryId: $("#categoryid").val(),
+            description: $("#description").val()
+        };
+
+        let productJson = JSON.stringify(product);
+
+        $.ajax({
+            url: `https://localhost:7034/api/Product/${id}`,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: productJson,
+            success: function (response) {
+                alert("Cập nhật xong !!");
+                loadproducts();
+                resetForm();
+                console.log(response);
+            },
+            error: function (xhr, status, error) {
+                alert("Lỗi cập nhật sản phẩm: " + xhr.responseText);
+                console.error(error);
+            }
+        })
+    }
+    else console.log("Lỗi nhập dữ liệu")
+})
+
+//Delete
+function deleteProduct(id) {
+    if (confirm(`Xác nhận xoá?`)) {
+        $.ajax({
+            url: `https://localhost:7034/api/Product/${id}`,
+            type: 'DELETE',
+            success: function (response) {
+                alert('Xoá thành công');
+                loadproducts();
+            },
+            error: function (xhr, status, error) {
+                alert("Lỗi khi xoá: " + xhr.responseText);
+                console.error(error);
+            }
+        })
+    }
+}
+
+// Hàm cho Button huỷ Update
+function resetForm() {
+    document.getElementById("create").style.display = "inline";
+    document.getElementById("update").style.display = "none";
+    document.getElementById("backToCreate").style.display = "none";
+
+    $("#idProduct").val("")
+    $("#imageTextNameFileHidden").val("");
+    $("#name").val("");
+    $("#price").val("");
+    $("#quantity").val("");
+    $("#description").val("");
+    $("#brandid").val("");
+    $("#categoryid").val("");
+    $("#imageFile").val("");
+    $("#imageProduct").empty();
+}
+
+
+//Điều kiện nhập Form
 function validateInput() {
     $(".error-message").text("");
     let isValid = true;
 
-    $(".form .form-control").each(function () {
+    $(".form .form-notnull").each(function () {
         if ($(this).val().trim() === "") {
-            $(this).next(".error-message").text("Không được để trống")
+            $(this).next(".error-message").text("không được để trống")
             isValid = false;
-        } 
+        }
     })
+
+    let brandId = $("#brandid").val();
+    if (brandId === "") {
+        $("#brandid").next(".error-message").text("Vui lòng chọn thương hiệu");
+        isValid = false;
+    }
+
+    let categoryId = $("#categoryid").val();
+    if (categoryId === "") {
+        $("#categoryid").next(".error-message").text("Vui lòng chọn loại sản phẩm");
+        isValid = false;
+    }
+
+    if ($("#imageTextNameFileHidden").val() === "") {
+        let imageName = $("#imageFile").val().split("\\").pop();
+        if (imageName === "") {
+            $("#imageFile").next(".error-message").text("Vui lòng thêm file ảnh");
+            isValid = false;
+        }
+    }
 
     let name = $("#name").val();
     if (name.length > 20) {
@@ -105,26 +252,5 @@ function validateInput() {
         isValid = false;
     }
 
-    let brandId = $("#brandid").val();
-    if (brandId === "") {
-        $("#brandid").next(".error-message").text("Vui lòng chọn thương hiệu");
-        isValid = false;
-    }
-
-    let categoryId = $("#categoryid").val();
-    if (categoryId === "") {
-        $("#categoryid").next(".error-message").text("Vui lòng chọn loại sản phẩm");
-        isValid = false;
-    }
-
-    let imageName = $("#imageFile").val().split("\\").pop();
-    if (imageName === "") {
-        $("#imageFile").next(".error-message").text("Vui lòng thêm file ảnh");
-        isValid = false;
-    }
     return isValid;
 }
-
-$("#test").click(function () {
-    validateInput();
-})
